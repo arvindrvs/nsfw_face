7ce8ac266caf917804710f465688d25ed869fe5e
Arvind_rvs <arvindrvs21@gmail.com>
2018-07-04 19:36:31 +0530

    Add files via upload

---
+++ cache2.py
@@ -0,0 +1,30 @@
+import subprocess
+import os
+from pathlib import Path
+import glob
+
+os.remove('text.txt')
+
+os.system('E:/ChromeCacheView.exe /scomma D:/Studies/nsfw_test/text.txt')
+
+            
+fo = open("text.txt", "r")
+while True:
+    line = fo.readline()
+    if not line: break
+    data=line.split(",",2)
+    if "data_2  [12288]" in line:
+        print(data[0])
+        print(data[1])
+        break
+
+cmd2 = subprocess.Popen('cmd.exe /C E:\ChromeCacheView.exe /copycache "'+data[1]+'" "image/jpeg" /CopyFilesFolder "D:\check" /UseWebSiteDirStructure 0')
+names = glob.glob("D:/Studies/nsfw_test/*.jpg")
+for i in names:
+    if i.startswith('faces') or i.startswith('server') or i.startswith('output'):
+        continue
+    img = i
+    break
+
+
+cmd3 = subprocess.Popen('cmd.exe /C python nsfw.py -m data/open_nsfw-weights.npy -u "'+data[1]+'" "'+img+'"')
